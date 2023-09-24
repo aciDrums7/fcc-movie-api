aa1c3ddaee66883af6d698aba416adbc690c3d3f
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-09-24 16:35:37 +0000

    Writing The First Endpoint

--- fcc-movie-api/src/main/java/org/freecodecamp/acidrums7/fccmovieapi/FccMovieApiApplication.java
+++ fcc-movie-api/src/main/java/org/freecodecamp/acidrums7/fccmovieapi/FccMovieApiApplication.java
@@ -2,12 +2,20 @@ package org.freecodecamp.acidrums7.fccmovieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FccMovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FccMovieApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World!";
 	}
 
 }
