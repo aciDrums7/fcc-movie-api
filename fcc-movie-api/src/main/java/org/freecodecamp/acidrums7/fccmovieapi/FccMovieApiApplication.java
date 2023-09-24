6a2b590904342ce712c448ec5e7151ad85dbadf2
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-09-24 16:30:18 +0000

    Created Spring Boot Gradle Project + VS Code configs

---
+++ fcc-movie-api/src/main/java/org/freecodecamp/acidrums7/fccmovieapi/FccMovieApiApplication.java
@@ -0,0 +1,13 @@
+package org.freecodecamp.acidrums7.fccmovieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FccMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FccMovieApiApplication.class, args);
+	}
+
+}
---
+++ fcc-movie-api/src/test/java/org/freecodecamp/acidrums7/fccmovieapi/FccMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.freecodecamp.acidrums7.fccmovieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FccMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
