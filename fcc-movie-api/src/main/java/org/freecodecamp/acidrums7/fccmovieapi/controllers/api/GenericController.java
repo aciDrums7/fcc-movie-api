528f794672c54ffb8aaf3e6e5123a6ebf7f19f37
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-10-01 21:58:30 +0000

    Added CORS annotations

--- fcc-movie-api/src/main/java/org/freecodecamp/acidrums7/fccmovieapi/controllers/api/GenericController.java
+++ fcc-movie-api/src/main/java/org/freecodecamp/acidrums7/fccmovieapi/controllers/api/GenericController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,19 +15,24 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 public interface GenericController<T, ID> {
 
+    @CrossOrigin
     @GetMapping(value = "/", produces = "application/json")
     public ResponseEntity<List<T>> findAll();
 
+    @CrossOrigin
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<T> findById(@PathVariable ID id);
 
+    @CrossOrigin
     @PostMapping(value = "/", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<T> post(@RequestBody T body);
 
+    @CrossOrigin
     @PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<T> put(@PathVariable ID id, @RequestBody T body);
 
+    @CrossOrigin
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> delete(@PathVariable ID id);
